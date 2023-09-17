8c4340ef08c71c500266bfa58222881d9a5c0c5a
SebastianGonzalezP <juanprieto20102002@gmail.com>
2023-09-16 21:13:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,27 @@ const entrenamientoController = require("./controllers/entrenamientoController")
 const { send } = require("process");
 
 // INDEX
-app.get('/',indexController.getIndex);
+app.get('/',(req,res)=>{
+  const data = {
+    userData:req.session.userData || null,
+    id_usuario:req.session.id_usuario || null,
+    usuario:req.session.usuario || null,
+    usuario_img:req.session.usuario_img || null,
+    tipo_indent:req.session.tipo_indent || null,
+    identificacion:req.session.identificacion || null,
+    correo:req.session.correo || null,
+    celular:req.session.celular || null,
+    genero:req.session.genero || null,
+    direccion:req.session.direccion || null,
+    telefono:req.session.telefono || null,
+    rol:req.session.rol || null,
+    usuario_img:req.session.usuario_img || null
+  }
+  if(data.usuario != null && data.direccion == null)
+  res.redirect('/usuario/informacion');
+  else
+  res.render('index', { data });
+});
 //
 
 //REGISTRO
