c20c6c98c5842c73cbdf2a441ee2f14d996c0947
SebastianGonzalezP <juanprieto20102002@gmail.com>
2023-10-14 17:09:11 -0500

    eliminación del front

--- public/js/modals.js
+++
@@ -1,9 +0,0 @@
-function openModal(id){
-    const modal = document.getElementById(id);
-    modal.classList.add('modal-show');
-}
-
-function closeModal(id){
-    const modal = document.getElementById(id);
-    modal.classList.remove('modal-show');
-}
