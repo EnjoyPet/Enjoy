da1196a4b5ed057c3fc23d8fc4fd6275001aa293
SebastianGonzalezP <juanprieto20102002@gmail.com>
2023-10-23 13:24:55 -0500

    distinciones de inicio de sesion

--- app.js
+++ app.js
@@ -1,155 +1,234 @@
 const path = require("path");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const controllers = require('./controllers.js');
-const conection = require('./db.js');
-
+const controllers = require("./controllers.js");
+const conection = require("./db.js");
 
 global.dotenv = require("dotenv");
-dotenv.config({ path: './env/.env' });
+dotenv.config({ path: "./env/.env" });
 const port = process.env.PORT || 3000;
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  service: "Gmail",
   auth: {
-      user: process.env.MAILER_MAIL,
-      pass: process.env.MAILER_PASS
-  }
+    user: process.env.MAILER_MAIL,
+    pass: process.env.MAILER_PASS,
+  },
 });
 
 global.transporter = transporter;
 
 const express = require("express");
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
 
-const multer = require('multer');
-const sharp = require('sharp');
-
+const multer = require("multer");
+const sharp = require("sharp");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'PantallaZapatoMause2213',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 30 * 60 * 1000,
-    // secure: true, descomentar cunado tenga https
-    httpOnly: true,
-    sameSite: 'strict'
-  }
-}));
+app.use(
+  session({
+    secret: "PantallaZapatoMause2213",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 30 * 60 * 1000,
+      // secure: true, descomentar cunado tenga https
+      httpOnly: true,
+      sameSite: "strict",
+    },
+  })
+);
 
 // Ahora puedes usar la variable modelsPath en otros scripts
 
 const { send } = require("process");
 
 // INDEX
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   const data = {
-    userData:req.session.userData || null,
-    id_usuario:req.session.id_usuario || null,
-    usuario:req.session.usuario || null,
-    usuario_img:req.session.usuario_img || null,
-    tipo_indent:req.session.tipo_indent || null,
-    identificacion:req.session.identificacion || null,
-    correo:req.session.correo || null,
-    celular:req.session.celular || null,
-    genero:req.session.genero || null,
-    direccion:req.session.direccion || null,
-    telefono:req.session.telefono || null,
-    rol:req.session.rol || null,
-    usuario_img:req.session.usuario_img || null
-  }
-  if(data.usuario != null && data.direccion == null)
-  res.redirect('/usuario/informacion');
-  else
-  res.render('index', { data });
+    userData: req.session.userData || null,
+    id_usuario: req.session.id_usuario || null,
+    usuario: req.session.usuario || null,
+    usuario_img: req.session.usuario_img || null,
+    tipo_indent: req.session.tipo_indent || null,
+    identificacion: req.session.identificacion || null,
+    correo: req.session.correo || null,
+    celular: req.session.celular || null,
+    genero: req.session.genero || null,
+    direccion: req.session.direccion || null,
+    telefono: req.session.telefono || null,
+    rol: req.session.rol || null,
+    usuario_img: req.session.usuario_img || null,
+  };
+  if (data.usuario != null && data.direccion == null)
+    res.redirect("/usuario/informacion");
+  else res.render("index", { data });
 });
 //
 
 //REGISTRO
-app.get('*/cuenta/registrarse',controllers.UserController.rederizarformderegistro);
-app.post('/registrar-usuario',controllers.UserController.registrarUsuario);
-app.post('*/verificar-mail',controllers.UserController.verificarCorreo);
+app.get(
+  "*/cuenta/registrarse",
+  controllers.UserController.rederizarformderegistro
+);
+app.post("/registrar-usuario", controllers.UserController.registrarUsuario);
+app.post("*/verificar-mail", controllers.UserController.verificarCorreo);
 //
 
 //INICIO
-app.get('*/cuenta/iniciar-sesion',controllers.UserController.rederizarformdeinicio);
-app.post('*/inciar',controllers.UserController.iniciarSesion);
-app.get('*/usuario/recuperar-contrasenia',controllers.UserController.RecuperarContrasenia);
-app.post('*/usuario/recuperar-contrasenia/verificar',controllers.UserController.renderizarverificarCodContrasenia);
-app.post('*/usuario/recuperar-contrasenia/verificar/actualizar-contrasenia',controllers.UserController.actualizarContraseniaOlvidada);
+app.get(
+  "*/cuenta/iniciar-sesion",
+  controllers.UserController.rederizarformdeinicio
+);
+app.post("*/inciar", controllers.UserController.iniciarSesion);
+app.get(
+  "*/usuario/recuperar-contrasenia",
+  controllers.UserController.RecuperarContrasenia
+);
+app.post(
+  "*/usuario/recuperar-contrasenia/verificar",
+  controllers.UserController.renderizarverificarCodContrasenia
+);
+app.post(
+  "*/usuario/recuperar-contrasenia/verificar/actualizar-contrasenia",
+  controllers.UserController.actualizarContraseniaOlvidada
+);
 
 //
 
 //CERRAR
-app.get('*/usuario/cerrar-sesion',controllers.UserController.cerrarSesion);
+app.get("*/usuario/cerrar-sesion", controllers.UserController.cerrarSesion);
 //
 
 //IFORMACION DE LA CUENTA
-app.get('*/usuario/informacion',controllers.acountController.renderAcountInfo);
-app.get('/usuario/informacion/actualizar/:objetivo',controllers.acountController.renderActializacionForm);
-app.get('/usuario/carrito',controllers.acountController.renderizarCarrito);
-app.get('/usuario/productos',controllers.acountController.renderizarProductos);
+app.get("*/usuario/informacion", controllers.acountController.renderAcountInfo);
+app.get(
+  "/usuario/informacion/actualizar/:objetivo",
+  controllers.acountController.renderActializacionForm
+);
+app.get("/usuario/carrito", controllers.acountController.renderizarCarrito);
+app.get("/usuario/productos", controllers.acountController.renderizarProductos);
 
-app.post('/verificar-actualizar-correo',controllers.acountController.verificarCodigo);
+app.post(
+  "/verificar-actualizar-correo",
+  controllers.acountController.verificarCodigo
+);
 
-app.post('/actualizar-correo',controllers.acountController.actualizarCorreo);
-app.post('/actualizar-direccion',controllers.acountController.actualizarDireccion);
-app.post('/actualizar-nombre',controllers.acountController.actualizarNombre);
-app.post('/actualizar-contrasenia',controllers.acountController.actualizarContrasenia);
-app.post('/actualizar-celular',controllers.acountController.actualizarCelular);
-app.post('/actualizar-identificacion',controllers.acountController.actualizarIdentificacion);
-app.post('/actualizar-telefono',controllers.acountController.actualizarTelefono);
-app.post('/actualizar-imagen_usuario', upload.single('imagen_post'),controllers.acountController.actualizarImagen);
+app.post("/actualizar-correo", controllers.acountController.actualizarCorreo);
+app.post(
+  "/actualizar-direccion",
+  controllers.acountController.actualizarDireccion
+);
+app.post("/actualizar-nombre", controllers.acountController.actualizarNombre);
+app.post(
+  "/actualizar-contrasenia",
+  controllers.acountController.actualizarContrasenia
+);
+app.post("/actualizar-celular", controllers.acountController.actualizarCelular);
+app.post(
+  "/actualizar-identificacion",
+  controllers.acountController.actualizarIdentificacion
+);
+app.post(
+  "/actualizar-telefono",
+  controllers.acountController.actualizarTelefono
+);
+app.post(
+  "/actualizar-imagen_usuario",
+  upload.single("imagen_post"),
+  controllers.acountController.actualizarImagen
+);
 
-app.post('/eliminar-imagen_usuario',controllers.acountController.eliminarImagen);
+app.post(
+  "/eliminar-imagen_usuario",
+  controllers.acountController.eliminarImagen
+);
 //
 
 //PRODUCTOS
-app.get('/productos/:categoria/:pagina',controllers.productosController.renderProductos);
-app.get('/ver/producto/:id_producto',controllers.productosController.renderInfoProducto);
-app.get('/producto/comprar/:id_producto',controllers.productosController.comprarProducto);
-app.get('/producto/al-carro/:id_producto',controllers.productosController.productoAlCarro);
-app.get('/carrito/:id_carrito/producto/comprar/:id_producto',controllers.productosController.comprarCarrito);
-app.get('/carrito/:id_carrito/producto/eliminar/:id_producto',controllers.productosController.eliminarCarrito);
+app.get(
+  "/productos/:categoria/:pagina",
+  controllers.productosController.renderProductos
+);
+app.get(
+  "/ver/producto/:id_producto",
+  controllers.productosController.renderInfoProducto
+);
+app.get(
+  "/producto/comprar/:id_producto",
+  controllers.productosController.comprarProducto
+);
+app.get(
+  "/producto/al-carro/:id_producto",
+  controllers.productosController.productoAlCarro
+);
+app.get(
+  "/carrito/:id_carrito/producto/comprar/:id_producto",
+  controllers.productosController.comprarCarrito
+);
+app.get(
+  "/carrito/:id_carrito/producto/eliminar/:id_producto",
+  controllers.productosController.eliminarCarrito
+);
 //
 
 //VENTAS
-app.get('/vender',controllers.ventasController.renderVentasForm);
-app.post('/PublicarUnaVenta', upload.single('imagen_post'),controllers.ventasController.publicarVenta);
+app.get("/vender", controllers.ventasController.renderVentasForm);
+app.post(
+  "/PublicarUnaVenta",
+  upload.single("imagen_post"),
+  controllers.ventasController.publicarVenta
+);
 //
 
 //TIPS DE ENTRENAMIENTO
-app.get('/entrenamiento/ensenar',controllers.entrenamientoController.renderensenarForm);
-app.get('/entrenamiento/aprender',controllers.entrenamientoController.renderAprender);
-app.post('/hacerUnPost' ,upload.single('imagen_post'),controllers.entrenamientoController.hacerPost);
-app.put('/entrenamiento/aprender/puntuar/noMeGusta/:id', (req, res) => {
+app.get(
+  "/entrenamiento/ensenar",
+  controllers.entrenamientoController.renderensenarForm
+);
+app.get(
+  "/entrenamiento/aprender",
+  controllers.entrenamientoController.renderAprender
+);
+app.post(
+  "/hacerUnPost",
+  upload.single("imagen_post"),
+  controllers.entrenamientoController.hacerPost
+);
+app.put("/entrenamiento/aprender/puntuar/noMeGusta/:id", (req, res) => {
   const postId = req.params.id;
 
   const sqlQuery = `UPDATE post SET noMeGusta_post = noMeGusta_post + 1 WHERE id_post = ?`;
   conection.query(sqlQuery, [postId], (err, result) => {
     if (err) {
-      res.status(500).json({ error: 'No se pudo actualizar la base de datos.' });
+      res
+        .status(500)
+        .json({ error: "No se pudo actualizar la base de datos." });
     } else {
-      res.status(200).json({ message: 'No Me Gusta actualizado en la base de datos.' });
+      res
+        .status(200)
+        .json({ message: "No Me Gusta actualizado en la base de datos." });
     }
   });
 });
-app.put('/entrenamiento/aprender/puntuar/meGusta/:id', (req, res) => {
+app.put("/entrenamiento/aprender/puntuar/meGusta/:id", (req, res) => {
   const postId = req.params.id;
 
   const sqlQuery = `UPDATE post SET meGusta_post = meGusta_post + 1 WHERE id_post = ?`;
   conection.query(sqlQuery, [postId], (err, result) => {
     if (err) {
-      res.status(500).json({ error: 'No se pudo actualizar la base de datos.' });
+      res
+        .status(500)
+        .json({ error: "No se pudo actualizar la base de datos." });
     } else {
-      res.status(200).json({ message: 'Me Gusta actualizado en la base de datos.' });
+      res
+        .status(200)
+        .json({ message: "Me Gusta actualizado en la base de datos." });
     }
   });
 });
@@ -157,31 +236,46 @@ app.put('/entrenamiento/aprender/puntuar/meGusta/:id', (req, res) => {
 
 //adopciones
 
-app.get('/usuario/adopciones/adoptar/:categoria/:filtro/:pagina',controllers.adopcionesController.rendermascotas)
-app.get('/usuario/adopciones/dar',controllers.adopcionesController.rendermascotasform)
-app.post('/usuario/adopciones/dar/registrarmascota',upload.array('imagen_mascota', 3),controllers.adopcionesController.registrarmascota)
+app.get(
+  "/usuario/adopciones/adoptar/:categoria/:filtro/:pagina",
+  controllers.adopcionesController.rendermascotas
+);
+app.get(
+  "/usuario/adopciones/dar",
+  controllers.adopcionesController.rendermascotasform
+);
+app.post(
+  "/usuario/adopciones/dar/registrarmascota",
+  upload.array("imagen_mascota", 3),
+  controllers.adopcionesController.registrarmascota
+);
 
-app.get('/ver/mascota/:idmascota',controllers.adopcionesController.verInfoAvanzada);
+app.get(
+  "/ver/mascota/:idmascota",
+  controllers.adopcionesController.verInfoAvanzada
+);
 
-app.get('/usuario/adoptar/mascota/:idmascota/:iddueno',controllers.adopcionesController.adoptar);
+app.get(
+  "/usuario/adoptar/mascota/:idmascota/:iddueno",
+  controllers.adopcionesController.adoptar
+);
 
 //
 
 //Sin Implementar
-app.get('/Pronto-Implementado',controllers.acountController.mostrarMensaje);
+app.get("/Pronto-Implementado", controllers.acountController.mostrarMensaje);
 
 //
-
 
 app.listen(port, () => {
   console.log(`Levantando el Servidor ${port}`);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/resources', express.static('public'));
-app.set('views', path.join(__dirname, 'views'))
-app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/resources", express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('¡Algo salió mal!');
+  res.status(500).send("¡Algo salió mal!");
 });
