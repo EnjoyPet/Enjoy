cbc4fc5d05116262968d0985aee4ac609ba70265
EnjoyPet <144075663+EnjoyPet@users.noreply.github.com>
2023-10-27 11:06:09 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,25 @@
 // db.js
 
 global.dotenv = require("dotenv");
-dotenv.config({ path: './env/.env' });
+dotenv.config({ path: "./env/.env" });
 
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Configuración de la conexión a la base de datos
 const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 // Conectar a la base de datos
 conexion.connect((err) => {
   if (err) {
-    console.error('Error al conectar a la base de datos: ' + err.message);
+    console.error("Error al conectar a la base de datos: " + err.message);
   } else {
-    console.log('Conexión a la base de datos establecida');
+    console.log("Conexión a la base de datos establecida");
   }
 });
 
